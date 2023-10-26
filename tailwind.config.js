66d75ed6467bab986965d07b7ab0a37dab88f586
adiatna sukmana <dyatna.id@gmail.com>
2023-10-26 17:59:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#045E2A",
+        secondary: "#057335",
+        accent: "#00923F",
+      },
+    },
+  },
+  plugins: [],
+};
